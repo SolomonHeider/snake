3fd034a8b6065bc85495aa6278f034efcaa73934
SolomonHeider <dariashalashova@yandex.ru>
2020-07-13 11:47:54 +0300

    lists again

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -17,7 +17,7 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>;
+            List<int> numList = new List<int>();
             numList.Add(0);
             numList.Add(1);
             numList.Add(2);
@@ -33,7 +33,7 @@ namespace Snake
 
             numList.RemoveAt(0);
 
-            List<Point> pList = new List<Point>;
+            List<Point> pList = new List<Point>();
             pList.Add(p1);
             pList.Add(p2);
 
